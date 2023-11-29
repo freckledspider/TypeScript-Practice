9a479a0c60fdfa0bc136473e51294c8638ddb92e
bri <bcamesi@gmail.com>
2023-11-29 15:32:51 -0500

    union types

--- TS-Tutorial-Beginners/index.ts
+++ TS-Tutorial-Beginners/index.ts
@@ -62,13 +62,44 @@
 // ----------------------------------------------
 // objects
 
-let employee:  {
-    readonly id: number,
-    name?: string,
-    retire: (date: Date) => void
-} = { 
-    id: 1, 
-    name: 'Mosh',
-    retire: (date: Date) => {
-        console.log(date)
-    } };
+// let employee:  {
+//     readonly id: number,
+//     name?: string,
+//     retire: (date: Date) => void
+// } = { 
+//     id: 1, 
+//     name: 'Mosh',
+//     retire: (date: Date) => {
+//         console.log(date)
+//     } };
+
+// ----------------------------------------------
+// advanced types
+
+// type Employee = {
+//     readonly id: number,
+//     name?: string,
+//     retire: (date: Date) => void
+    
+// }
+
+// let employee: Employee = { 
+//     id: 1, 
+//     name: 'Mosh',
+//     retire: (date: Date) => {
+//         console.log(date)
+//     } };
+
+// union types
+
+function kgToLbs(weight: number | string): number {
+    // narrowing
+    if (typeof weight === 'number') 
+        return weight * 2.2;
+        else {
+            return parseInt(weight) * 2.2;
+        }
+    };
+
+kgToLbs(10);
+kgToLbs('10kg');
