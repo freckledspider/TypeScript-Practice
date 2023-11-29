5bf1b95a6edbeaa4009e6f295a079a9d3196584e
bri <bcamesi@gmail.com>
2023-11-29 14:40:14 -0500

    TS compiler

---
+++ TS-Tutorial-Beginners/index.js
@@ -0,0 +1,2 @@
+// TypeScript Tutorial for Beginners by Programming with Mosh on YouTube
+var age = 20;
---
+++ TS-Tutorial-Beginners/index.ts
@@ -0,0 +1,4 @@
+// TypeScript Tutorial for Beginners by Programming with Mosh on YouTube
+
+let age: number = 20;
+
