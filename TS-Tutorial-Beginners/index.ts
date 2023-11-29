ec0a0709524f1bf7129055ac7f7d5b7c0721d086
bri <bcamesi@gmail.com>
2023-11-29 15:02:08 -0500

    any type

--- TS-Tutorial-Beginners/index.ts
+++ TS-Tutorial-Beginners/index.ts
@@ -1,7 +1,16 @@
 // TypeScript Tutorial for Beginners by Programming with Mosh on YouTube
 
-let age: number = 20;
+// let age: number = 20;
 
-if (age < 50)
-    age += 10;
-console.log(age)
+// if (age < 50)
+//     age += 10;
+// console.log(age)
+
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+
+function render(document) {
+    console.log(document)
+}
